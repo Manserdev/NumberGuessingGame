935ed746ea185deae66f9b2b1f9a627aae1b36fd
Tim Manser <manserdev@protonmail.com>
2024-03-15 19:30:40 +1100

    First and last commit for this small project

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess");
+
+        //Created an empty string to store user input
+        let mut guess = String::new();
+
+        /*Method which takes the users input and stores it in
+    our empty string variable*/
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
